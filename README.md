aaabf08278c213b7007a9c3366adb81582b45801
Ryan X. Charles <ryanxcharles@gmail.com>
2015-12-05 11:27:42 -0800

    switch to "standard" implementation
    I've realized there is actually a "standard" implementation of spawn, so it
    makes sense to use that rather than Jack Archibald's implementation. They are
    aesthetically different, but almost exactly the same computationally, except
    the optional "self" in the standard implementation.
    
    The "standard" is found here:
    http://tc39.github.io/ecmascript-asyncawait/#desugaring

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 asink
 =====
 Like async/await, but asink/yield. A placeholder until async/await are
-standardized and included in node.js and web browsers.
-
-[This code was originally written by Jake
-Archibald](https://gist.github.com/jakearchibald/31b89cba627924972ad6). Ryan X.
-Charles gave it a funny name and then took all the credit.
+standardized and included in node.js and web browsers. "asink" is exactly the
+same thing as
+["spawn"](http://tc39.github.io/ecmascript-asyncawait/#desugaring), but with a
+slightly better name.
 
 How To Use
 ----------
@@ -30,7 +29,7 @@ function myfunc () {
     // do something else
     yield aPromise1
     // do yet another thing
-    yield aPromise2
+    return aPromise2
   })
 }
 ```
@@ -38,7 +37,7 @@ Code written with asink looks nicer and is easier to write and understand than
 without it. It is especially nice when there is a lot of logic with promises,
 or when there are errors that go inside try/catch blocks. It is almost the same
 as the async/await pattern of the upcoming version of javascript, ES7, but
-works today in node.js and (most) web browsers.
+works today in node.js and (most modern) web browsers.
 
 The same code written in the as-yet-unworking-and-unstandardized-es7 is:
 ```
@@ -47,7 +46,7 @@ async function myfunc () {
   // do something else
   await aPromise1
   // do yet another thing
-  await aPromise2
+  return aPromise2
 }
 ```
 asink lets you have most of the readability and writability gains in a form
--- index.js
+++ index.js
@@ -2,45 +2,49 @@
  * asink
  * =====
  *
- * This is a tool for repeatedly calling the .thens of promises yielded by a
- * generator. Basically, this makes it possible to write asynchronous,
- * promisified code with normal try/catches that look just like synchronous
- * code. It creates shorter and easier to understand code. Hypothetically,
- * there will be a feature in the next version of javascript, ES7, called
- * "async functions", which do exactly what asink does. When/if that happens
- * and we can access it in node, we can simply remove all calls to asink and
- * our code should behave in the same way.
- *
- * This code was written by Jake Archibald [1], and moved to a module and renamed
- * "asink" by Ryan X. Charles.
+ * asink is the same thing as, or a rename of, spawn. spawn in turn is a tool
+ * for repeatedly calling the .thens of promises yielded by a generator.
+ * Basically, this makes it possible to write asynchronous, promisified code
+ * with normal try/catches that look just like synchronous code. It creates
+ * shorter and easier to understand code. Hypothetically, there will be a
+ * feature in the next version of javascript, ES7, called "async functions",
+ * which do exactly what asink does. When/if that happens and we can access it
+ * in node, we can simply remove all calls to asink and our code should behave
+ * in the same way.
  *
  * See:
- * https://github.com/lukehoban/ecmascript-asyncawait
+ * http://tc39.github.io/ecmascript-asyncawait/
+ * https://github.com/tc39/ecmascript-asyncawait
  * https://gist.github.com/jakearchibald/31b89cba627924972ad6
  * http://www.html5rocks.com/en/tutorials/es6/promises/
- * https://github.com/tc39/ecmascript-asyncawait
- * http://tc39.github.io/ecmascript-asyncawait/
- *
- * [1] https://gist.github.com/jakearchibald/31b89cba627924972ad6
+ * https://blogs.windows.com/msedgedev/2015/09/30/asynchronous-code-gets-easier-with-es2016-async-function-support-in-chakra-and-microsoft-edge/
  */
 'use strict'
-function asink (generatorFunc) {
-  function continuer (verb, arg) {
-    var result
-    try {
-      result = generator[verb](arg)
-    } catch (err) {
-      return Promise.reject(err)
+function spawn (genF, self) {
+  return new Promise(function (resolve, reject) {
+    var gen = genF.call(self)
+    function step (nextF) {
+      var next
+      try {
+        next = nextF()
+      } catch (e) {
+        // finished with failure, reject the promise
+        reject(e)
+        return
+      }
+      if (next.done) {
+        // finished with success, resolve the promise
+        resolve(next.value)
+        return
+      }
+      // not finished, chain off the yielded promise and `step` again
+      Promise.resolve(next.value).then(function (v) {
+        step(function () { return gen.next(v) })
+      }, function (e) {
+        step(function () { return gen.throw(e) })
+      })
     }
-    if (result.done) {
-      return result.value
-    } else {
-      return Promise.resolve(result.value).then(onFulfilled, onRejected)
-    }
-  }
-  var generator = generatorFunc()
-  var onFulfilled = continuer.bind(continuer, 'next')
-  var onRejected = continuer.bind(continuer, 'throw')
-  return onFulfilled()
+    step(function () { return gen.next(undefined) })
+  })
 }
-module.exports = asink
+module.exports = spawn
